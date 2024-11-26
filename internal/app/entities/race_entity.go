64f9a6e215c3db8dfaa6aa07d4611bcecfd760d0
Matheus <mthsbessac@gmail.com>
2024-11-26 07:30:28 -0300

    race services

---
+++ internal/app/entities/race_entity.go
@@ -0,0 +1,13 @@
+package entities
+
+import "time"
+
+type Race struct {
+	ID            int           `json:"id" gorm:"primaryKey"`
+	RunnerID      int           `json:"runner_id"`
+	Distance      float64       `json:"distance"`
+	Pace          float64       `json:"pace"`
+	Duration      time.Duration `json:"duration"`
+	TypeOfRunning string        `json:"type"`
+	Date          time.Time     `json:"date"`
+}
--- internal/app/handlers/race_handler.go
+++ internal/app/handlers/race_handler.go
@@ -1,1 +1,20 @@
 package handlers
+
+import (
+	"RunningTracker/internal/app/services"
+
+	"github.com/labstack/echo/v4"
+)
+
+type RaceHandler struct {
+	service services.RaceService
+}
+
+func NewRaceHandler(service services.RaceService) *RaceHandler {
+	return &RaceHandler{service: service}
+
+}
+
+func (h *RaceHandler) CreateRace(c echo.Context) error {
+
+}
